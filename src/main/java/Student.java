318d452c45b66ef290bb9f702fa154c144578e1d
Jose De La Luz <Jdelaluz01@gmail.com>
2023-08-16 13:50:34 -0500

    Student obj with methods

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -2,6 +2,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.ArrayList;
+import java.util.Scanner;
+
 @Getter
 @Setter
 public class Student {
@@ -12,5 +14,34 @@ public class Student {
         this.id = id;
         this.name = name;
         this.grades = grades;
+    }
+    public double getGradeAverage(ArrayList<Integer> grades){
+        double sum =0;
+        for(int grade: grades){
+            sum += grade;
+        }
+        return sum/ grades.size();
+    }
+    public ArrayList<Integer> updatedGrade(int searchedGrade){
+        for(int grade:  grades){
+            if (searchedGrade == grade){
+                Scanner sc = new Scanner(System.in);
+                System.out.println("You searched for " + searchedGrade + " enter the grade you want to replace it with");
+                int result =  sc.nextInt();
+                grades.remove(grade);
+                grades.add(result);
+            }
+        }
+        return grades;
+    }
+    public ArrayList<Integer> deleteGrade(int searchedGrade){
+        for(int grade:  grades){
+            if (searchedGrade == grade){
+                Scanner sc = new Scanner(System.in);
+                System.out.println("You searched for " + searchedGrade + " and it will be deleted");
+                grades.remove(grade);
+            }
+        }
+        return grades;
     }
 }
